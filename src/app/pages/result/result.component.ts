f4077d816182bd34bbb00990fe58fba1e9228293
gabialverga <gabrielaalverga@gmail.com>
2020-07-02 21:41:08 -0300

    /download

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChampionsService } from '../champions.service';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { merge } from 'rxjs';
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
+
 export class ResultComponent implements OnInit, OnDestroy {
   isCollapsed = true;
   champions = [];
@@ -13,6 +20,7 @@ export class ResultComponent implements OnInit, OnDestroy {
   patch;
   checkFinish = [];
   result = [];
+  numberChamp = 15;
   lanes = [
     { name: 'Meio', id: 'middle' },
     { name: 'Topo', id: 'top' },
@@ -36,7 +44,7 @@ export class ResultComponent implements OnInit, OnDestroy {
         console.log(this.busca.champs[i]);
         this.checkFinish.push(false);
         this.result.push({});
-        this.champService.getDataChampion(this.patch, this.busca.champs[i].champion, this.busca.champs[i].lane, this.busca.queue, this.busca.region, 15)
+        this.champService.getDataChampion(this.patch, this.busca.champs[i].champion, this.busca.champs[i].lane, this.busca.queue, this.busca.region, this.numberChamp)
           .then((resp: any) => {
             this.result[i] = resp;
             this.checkFinish[i] = true;
@@ -82,4 +90,51 @@ export class ResultComponent implements OnInit, OnDestroy {
       return true;
     }
   }
+
+  download() {
+    console.log(this.result);
+    let data = [];
+    let merges = [];
+
+    for (let i = 0; i < this.numberChamp + 3; i++) data.push([]);
+
+    for (let i = 0; i < (this.result.length * 2); i+=2) {
+      let c1 = i, c2 = i + 1;
+      merges.push({ s: { r: 0, c: c1 }, e: { r: 0, c: c2 } });
+      merges.push({ s: { r: 1, c: c1 }, e: { r: 1, c: c2 } });
+    }
+
+    console.log(merges);
+
+    this.result.forEach(r => {
+      merges.push();
+      data[0].push(this.getChampName(r.cid));
+      data[0].push('');
+      data[1].push(r.lane);
+      data[1].push('');
+      data[2].push('Forte');
+      data[2].push('Fraco');
+      for (let j = 0; j < this.numberChamp; j++) {
+        data[j + 3].push(this.getChampName(r.strong[j][0]) + " ( " + r.strong[j][1] + "% )");
+        data[j + 3].push(this.getChampName(r.weak[j][0]) + " ( " + r.weak[j][1] + "% )");
+      }
+    });
+    this.exportAsExcelFile(data, 'champions', merges);
+  }
+
+
+  public exportAsExcelFile(json, excelFileName, merges): void {
+    var ws = XLSX.utils.aoa_to_sheet(json);
+    ws['!merges'] = merges;
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "sheet1");
+    const wbout = XLSX.write(wb, { type: "array", bookType: "xlsx" });
+    let excelBuffer = new Blob([wbout], { type: "application/octet-stream" })
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
+  }
+
 }
